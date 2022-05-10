87e795f62eaf105b40b5bd8410ea1bfc53ad735c
ivan <vivan.volovik@gmail.com>
2022-05-10 12:38:28 +0300

    first raw implmentaion

---
+++ contracts/interfaces/IVault.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: Unlicense
+
+pragma solidity ^0.6.6;
+
+interface IVault {
+    function deposit(uint256, address) external returns (uint256);
+
+    function withdraw(
+        uint256,
+        uint256,
+        uint256,
+        address
+    ) external returns (uint256, uint256);
+
+    function timeRebalance(
+        bool,
+        uint256,
+        uint256,
+        uint256
+    ) external;
+
+    function getTotalAmounts() external view returns (uint256, uint256);
+}
